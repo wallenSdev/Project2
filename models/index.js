de4383db1caf9823e9f46f5000687ccba527b7ef
Kev1nJ <kevinjerome85@yahoo.com>
2023-10-17 17:11:36 -0400

    Models added, API keys added into script.js and commented out , model relations created

--- models/index.js
+++ models/index.js
@@ -1,1 +1,23 @@
-//Define associate between Ingredient, User, and Recipe Models
+const User = require('./User');
+const Ingredient = require('./Ingredient');
+const Recipe = require('./Recipe');
+
+User.hasMany(Recipe, {
+  foreignKey: 'user_id',
+});
+
+Recipe.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Recipe.belongsToMany(Ingredient, {
+  through: 'RecipeIngredient', 
+  foreignKey: 'recipe_id',
+});
+
+Ingredient.belongsToMany(Recipe, {
+  through: 'RecipeIngredient', 
+  foreignKey: 'ingredient_id',
+});
+
+module.exports = { User, Ingredient, Recipe };
--- script.js
+++ script.js
@@ -69,3 +69,26 @@ const ingredients = {
       });
     }
   }
+
+  //API
+
+  // function getSource(id) {
+  //     $.ajax({
+  //         url: "https://api.spoonacular.com/recipes/" + id + "/information?apiKey=ada030f7a07b411c83ea10178938e8de",
+  //         succes: function (res) {
+  //             document.getElementById("sourceLink").innerHTML = res.sourceUrl
+  //             document.getElementById("sourceLink").href = res.sourceUrl
+  //         }
+  //     });
+  // }
+
+  // function getRecipe(q) {
+  //     $.ajax({
+  //         url: "https://api.spoonacular.com/recipes/search?apiKey=ada030f7a07b411c83ea10178938e8de&number=8&query" + q,
+  //         success: function (res) {
+  //             document.getElementById("output").innerHTML = "<h1>" + res.results[0].title + "</h1><br><img src='" + res.baseUri + res.results[0].image + "' width='400' /> <br> ready in " + res.results[0].readyInMinutes + " minutes"
+  //             getsource(res.results[0].id)
+  //         }
+  //     })
+  // };
+
