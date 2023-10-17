cf04efb0b6ae52a55b6d2066edda61c08ddb781d
Kevin Jerome <137011939+Kev1nJ@users.noreply.github.com>
2023-10-17 17:48:44 -0400

    Update Ingredient.js

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 
 class Ingredient extends Model {}
 
-Painting.init(
+Ingredient.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -11,32 +11,9 @@ Painting.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    title: {
+    name: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    artist: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    exhibition_date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    filename: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    gallery_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'Recipe',
-        key: 'id',
-      },
     },
   },
   {
